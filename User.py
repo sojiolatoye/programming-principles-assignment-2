6cccde3493f350c57322995c0333f1c298dc17cc
sojiolatoye <you@example.com>
2023-10-12 12:52:31 -0400

    Fourth commit

--- User.py
+++ User.py
@@ -1,25 +1,29 @@
-from pizzaapp import u, p 
 class User:
     def __init__(self, name, email, address):
         self.name = name
-        self.email = email
+        self.email = email     
         self.address = address
+        
     
 class Pizza:
-    def __init__(self, size = "Small, Medium, Large, X-Large", price = "10, 12, 15, 18", numberofpizzas = []):
+    def __init__(self, size, price, numberofpizzas):
         self.size = size
         self.price = price
         self.numberofpizzas = numberofpizzas
     
     def total(self):
-        self.price = self.size + self.numberofpizzas
-        if p.numberofpizzas >= 3:
-            (self.price / 15) + self.price
+        self.price = self.price * self.numberofpizzas
+        if self.numberofpizzas >= 3:
+           self.price = self.price * 0.85
         return self.price
+       
 
+        
+        
 
     def __str__(self):
         return f'''You have ordered {self.numberofpizzas} {self.size} pizzas
         Your total after discount is {self.price}
-        Order will be delivered to {u.name} at {u.address}
-        Receipt will be emailed to {u.email}'''
+        Order will be delivered to {self.name} at {self.address}
+        Receipt will be emailed to {self.email}'''
+
--- pizzaapp.py
+++ pizzaapp.py
@@ -1,24 +1,44 @@
-from User import User, Pizza
-
-u = User()
-p = Pizza()
-
-u.name = input("What is your name?")
-u.email = input("What is your email address?")
-u.address = input("What is your  address?")
+from User import User, Pizza  
 
 
 
-p.size = input("What size pizza would you like Small/Medium/Large/X-Large?")
-if p.size == "Small":
-    print("One pizza of this size is $10")
-elif p.size == "Medium": 
-    print("One pizza of this size is $12")
-elif p.size == "Large":
-    print("One pizza of this size is $15")
-elif p.size == "X-Large":
-    print("One pizza of this size is $18")
-p.numberofpizzas= int(input("How many pizzas of this size would you like to order?"))
 
-u = User()
-p = Pizza()
+
+name1 = input("What is your name?")
+email1 = input("What is your email address?")
+address1 = input("What is your  address?")
+
+u = User(name1, email1, address1)
+print(u.name,u.email, u.address)
+
+
+size1 = input("What size pizza would you like Small/Medium/Large/X-Large?")
+if size1 == "Small":
+   ("One pizza of this size is $10")
+elif size1 == "Medium": 
+    ("One pizza of this size is $12")
+elif size1 == "Large":
+    ("One pizza of this size is $15")
+elif size1 == "X-Large":
+    ("One pizza of this size is $18")
+
+price1 = 0
+
+if size1 == "Small":
+    price1 = 10
+elif size1 == "Medium":
+    price1 = 12
+elif size1 == "Large":
+    price1 = 15
+elif size1 == "X-Large":
+    price1 = 18
+
+
+numberofpizzas1 = int(input("How many pizzas of this size would you like to order?"))
+
+p = Pizza(size1, price1, numberofpizzas1)
+print(p.size, p.total(), p.numberofpizzas)
+
+
+
+#print(p)
